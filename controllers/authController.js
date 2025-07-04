892910a9ef699d3adc44ddf2176db5932e0dde3c
unknown <mugalibhoomika@gmail.com>
2025-07-04 11:17:15 +0530

    Initial try

---
+++ controllers/authController.js
@@ -0,0 +1,94 @@
+const user = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const registerUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required",
+      });
+    }
+    const existingUser = await user.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+    const newUser = await user.create({
+      email,
+      password,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+    });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required",
+      });
+    }
+
+    const User = await user.findOne({ email });
+    if (!User) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, User.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    const token = jwt.sign(
+      { UserId: User._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 3600000,
+    });
+    res.status(200).json({
+      message: "Login successful",
+      user: {
+        id: User._id,
+        email: User.email,
+        role: User.role,
+      },
+      token: token,
+    });
+  } catch (error) {
+    console.error("Error logging in user:", error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+const logoutUser = (req, res, next) => {
+  res.clearCookie("token");
+  res.status(200).json({
+    message: "Logout successful",
+  });
+};
+module.exports = {
+  registerUser,
+  loginUser,
+  logoutUser,
+};
